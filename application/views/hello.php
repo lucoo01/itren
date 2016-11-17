1599a598155c54bfb36db2654895cd6ce6267b41
lucoo01 <425389019@qq.com>
2016-11-18 01:05:10 +0800

    init base

---
+++ application/views/hello.php
@@ -0,0 +1,3 @@
+<div >
+hello CI3
+</div>
---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>欢迎来到Loen主页</title>
+</head>
+<body>
+
+<div id="container">
+这里是Loen的主页
+</div>
+
+</body>
+</html>
